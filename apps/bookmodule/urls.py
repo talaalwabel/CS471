5d043afaf086b6fd2d2c30a8cb15b2f37828b8b1
talaalwabel <talaalwa28@gmail.com>
2025-10-26 10:07:46 +0300

    Lab6 t2

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -13,5 +13,6 @@ path('html5/links/', views.html5_links, name='html5_links'),
 path('html5/text/formatting/', views.text_formatting, name='text_formatting'),
 path('html5/listing/', views.html5_listing, name='html5_listing'),
 path('html5/tables/', views.html5_tables, name='html5_tables'),
-path('search', views.search_books, name='search_books'),
+path('search/', views.search_books, name='search_books'),
+
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -17,12 +17,35 @@ def html5_listing(request):
     return render(request, 'bookmodule/html5/listing.html')
 def html5_tables(request):
     return render(request, 'bookmodule/html5/tables.html')
+def __getBooksList(): 
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'} 
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'} 
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'} 
+    return [book1, book2, book3] 
+
+
 def search_books(request):
     if request.method == "POST":
-        keyword = request.POST.get("keyword")
-        option1 = request.POST.get("option1")
-        option2 = request.POST.get("option2")
-        print(keyword, option1, option2)
+        string = request.POST.get('keyword', '').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower():
+                contained = True
+            if not contained and isAuthor and string in item['author'].lower():
+                contained = True
+            if contained:
+                newBooks.append(item)
+        
+    
+        return render(request, 'layouts/bookList.html', {'books': newBooks})
+    
+    
     return render(request, 'layouts/search.html')
+
 
 
