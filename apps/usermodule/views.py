9a4cfd6c775724deefcee084a4bfe01df3cf5f20
talaalwabel <talaalwa28@gmail.com>
2025-12-07 10:53:56 +0300

    Lab12 t2

--- apps/usermodule/views.py
+++ apps/usermodule/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 def register_user(request):
@@ -16,3 +17,20 @@ def register_user(request):
         return redirect('/users/login')
 
     return render(request, 'usermodule/register.html')
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful!")
+            return redirect('/books/lab11/students/')   # ← المكان اللي ترجعين له بعد النجاح
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, 'usermodule/login.html')
